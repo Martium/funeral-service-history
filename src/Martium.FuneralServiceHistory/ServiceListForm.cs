80c2cd8da74b2d762ff62bef89351f22f7020172
Martynas Gedutis <martynasgedutis@gmail.com>
2020-12-21 18:59:49 +0200

    initial project added

---
+++ src/Martium.FuneralServiceHistory/ServiceListForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Martium.FuneralServiceHistory
+{
+    public partial class ServiceListForm : Form
+    {
+        public ServiceListForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
