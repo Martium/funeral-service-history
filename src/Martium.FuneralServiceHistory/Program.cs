68d62cbf431e0736b1a2810678044aa18b1302b3
Martynas Gedutis <martynasgedutis@gmail.com>
2020-12-23 10:40:29 +0200

    removed redundant comments

--- src/Martium.FuneralServiceHistory/Program.cs
+++ src/Martium.FuneralServiceHistory/Program.cs
@@ -8,9 +8,6 @@ namespace Martium.FuneralServiceHistory
     {
         private static readonly DatabaseInitializerRepository DatabaseInitializerRepository = new DatabaseInitializerRepository();
 
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
