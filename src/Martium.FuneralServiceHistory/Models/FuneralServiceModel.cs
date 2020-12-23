e11bab13173f54c910fa067853d3693f98e2e9cf
Martynas Gedutis <martynasgedutis@gmail.com>
2020-12-23 10:29:43 +0200

    added departed confession to table & recreate DB all the time

--- src/Martium.FuneralServiceHistory/Models/FuneralServiceModel.cs
+++ src/Martium.FuneralServiceHistory/Models/FuneralServiceModel.cs
@@ -4,8 +4,8 @@ namespace Martium.FuneralServiceHistory.Models
 {
     public class FuneralServiceModel
     {
-        public string ServiceDates { get; set; }
         public int OrderNumber { get; set; }
+        public string ServiceDates { get; set; }
         public string CustomerNames { get; set; }
         public string CustomerPhoneNumbers { get; set; }
         public string DepartedInfo { get; set; }
--- src/Martium.FuneralServiceHistory/Repositories/DatabaseInitializerRepository.cs
+++ src/Martium.FuneralServiceHistory/Repositories/DatabaseInitializerRepository.cs
@@ -9,7 +9,8 @@ namespace Martium.FuneralServiceHistory.Repositories
         {
             if (File.Exists(AppConfiguration.DatabaseFile))
             {
-                return;
+                // TODO: uncomment return below before real testing
+                //return;
             }
 
             if (!Directory.Exists(AppConfiguration.DatabaseFolder))
@@ -57,28 +58,29 @@ namespace Martium.FuneralServiceHistory.Repositories
             const string createFuneralServiceHistoryTableQuery =
                 @"                  
                   CREATE TABLE [FuneralServiceHistory] (
-                    [OrderNumber] [INTEGER] PRIMARY KEY AUTOINCREMENT,
-                    [OrderDate] [Date] NOT NULL,
-                    [CustomerNames] [nvarchar](800) NULL,
-                    [CustomerPhoneNumbers] [nvarchar](100) NOT NULL,
-	                [CustomerEmails] [nvarchar](800) NULL,
-	                [CustomerAddresses] [nvarchar](800) NULL,
-	                [ServiceDates] [nvarchar](150) NULL,
-	                [ServicePlaces] [nvarchar](300) NULL,
-	                [ServiceTypes] [nvarchar](300) NULL,
-                    [ServiceDuration] [decimal](18, 1) NULL,
-                    [ServiceMusiciansCount] [int] NULL,
-                    [ServiceMusicProgram] [nvarchar](200) NULL,
-                    [DepartedInfo] [nvarchar](810) NULL,
-	                [DepartedRemainsType] [nvarchar](50) NULL,
-	                [ServiceMusicianUnitPrices] [nvarchar](200) NULL,
-	                [ServiceDiscountPercentage] [int] NULL,
-	                [ServicePaymentAmount] [decimal](18, 2) NULL,
-	                [ServicePaymentCurrencyCode] [nvarchar](3) NULL,
-	                [ServicePaymentType] [nvarchar](100) NULL,
-	                [ServiceDescription] [nvarchar](1000) NULL,
-	                [ServiceReviewScore] [nvarchar](50) NULL,
-	                [ServiceReviewComments] [nvarchar](800) NULL
+                     [OrderNumber] [INTEGER] PRIMARY KEY AUTOINCREMENT,
+                     [OrderDate] [Date] NOT NULL,
+                     [CustomerNames] [nvarchar](800) NULL,
+                     [CustomerPhoneNumbers] [nvarchar](100) NOT NULL,
+                     [CustomerEmails] [nvarchar](800) NULL,
+                     [CustomerAddresses] [nvarchar](800) NULL,
+                     [ServiceDates] [nvarchar](150) NULL,
+                     [ServicePlaces] [nvarchar](300) NULL,
+                     [ServiceTypes] [nvarchar](300) NULL,
+                     [ServiceDuration] [decimal](18, 1) NULL,
+                     [ServiceMusiciansCount] [int] NULL,
+                     [ServiceMusicProgram] [nvarchar](200) NULL,
+                     [DepartedInfo] [nvarchar](810) NULL,
+                     [DepartedConfession] [nvarchar](100) NULL,
+                     [DepartedRemainsType] [nvarchar](50) NULL,
+                     [ServiceMusicianUnitPrices] [nvarchar](200) NULL,
+                     [ServiceDiscountPercentage] [int] NULL,
+                     [ServicePaymentAmount] [decimal](18, 2) NULL,
+                     [ServicePaymentCurrencyCode] [nvarchar](3) NULL,
+                     [ServicePaymentType] [nvarchar](100) NULL,
+                     [ServiceDescription] [nvarchar](1000) NULL,
+                     [ServiceReviewScore] [nvarchar](50) NULL,
+                     [ServiceReviewComments] [nvarchar](800) NULL
                   );
                 ";
             SQLiteCommand createFuneralServiceHistoryTableCommand = new SQLiteCommand(createFuneralServiceHistoryTableQuery, dbConnection);
@@ -90,8 +92,8 @@ namespace Martium.FuneralServiceHistory.Repositories
             const string fillFuneralServiceHistoryTableQuery =
                 @"BEGIN TRANSACTION;
 	                INSERT INTO 'FuneralServiceHistory' 
-                           (OrderDate, CustomerPhoneNumbers)
-	                VALUES ('2020-12-22 10:09:03', '+37062505181');
+                           (OrderDate, ServiceDates, CustomerNames, CustomerPhoneNumbers, DepartedInfo)
+	                VALUES ('2020-12-23 10:09:03', '2020-12-22 10:09:03', 'Martynas Gedutis', '+37062505181', 'Balandis Petras');
                 COMMIT;";
             SQLiteCommand fillFuneralServiceHistoryTableCommand = new SQLiteCommand(fillFuneralServiceHistoryTableQuery, dbConnection);
             fillFuneralServiceHistoryTableCommand.ExecuteNonQuery();
